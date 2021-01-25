3fbab4b48dda99b28f5d24090a5cc9a55bac65a6
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-01-25 22:40:28 +0100

    feat: select_from_nobel_quiz

--- select_from_nobel_quiz.sql
+++ select_from_nobel_quiz.sql
@@ -8,3 +8,32 @@ SELECT COUNT(subject) FROM nobel
    AND yr BETWEEN 1950 and 1960
 
 -- 3. Pick the code that shows the amount of years where no Medicine awards were given
+	SELECT COUNT(DISTINCT yr) FROM nobel
+		WHERE yr NOT IN (SELECT yr FROM nobel 
+			WHERE subject = 'Medicine')
+
+--4. Select the result that would be obtained from the following code:
+
+ 	SELECT subject, winner FROM nobel 
+		WHERE yr LIKE '196%' and winner LIKE 'Sir%'
+
+--5.Select the code which would show the year when neither a Physics or Chemistry award was given
+
+SELECT yr FROM nobel 
+		WHERE yr NOT IN 
+			(SELECT yr FROM nobel 
+				WHERE subject = 'Physics' OR subject = 'Chemistry')
+				or
+				WHERE subject IN ('Physics', 'Chemistry')
+--6. //Select the code which shows the years when a Medicine award was given but no Peace or Literature award was
+SELECT DISTINCT yr FROM nobel 
+		WHERE yr NOT IN (
+			SELECT yr FROM nobel
+				WHERE subject IN ('Peace', 'Literature')
+		)
+		AND subject = 'Medicine'
+--7.Pick the result that would be obtained from the following code:
+
+SELECT subject, COUNT(subject) FROM nobel
+		WHERE yr = '1960' 
+		GROUP BY subject
