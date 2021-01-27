e8edcb7999c50ddc97763d12f154c331aba8cefa
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-01-27 23:31:55 +0100

    select in select

---
+++ 3_select-in-select.sql
@@ -0,0 +1,13 @@
+--1. List each country name where the population is larger than 'Russia'.
+SELECT name FROM world 
+	WHERE population > (
+		SELECT population FROM world
+			WHERE name = 'Russia'
+	)
+
+--2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+SELECT name FROM world 
+	WHERE continent = 'Europe' AND
+		gdp/population > (
+			SELECT gdp/population FROM world 
+				WHERE name = 'United Kingdom'
